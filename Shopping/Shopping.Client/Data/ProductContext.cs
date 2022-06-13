6e3de3beedb2ce38057c7b7f1de6151020b627c2
ermanarisoy <ermanarisoy@outlook.com>
2022-06-13 14:49:44 +0300

    Initial

---
+++ Shopping/Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,36 @@
+﻿using Shopping.Client.Models;
+using System.Collections.Generic;
+
+namespace Shopping.Client.Data
+{
+    public static class ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product
+            {
+                Name = "Product 1",
+                Description = "Description 1",
+                ImageFile = "product-1.png",
+                Price = 950.00M,
+                Category = "Smart Phone"
+            },
+            new Product
+            {
+                Name = "Product 2",
+                Description = "Description 2",
+                ImageFile = "product-2.png",
+                Price = 850.00M,
+                Category = "Smart Phone"
+            },
+            new Product
+            {
+                Name = "Product 3",
+                Description = "Description 3",
+                ImageFile = "product-3.png",
+                Price = 750.00M,
+                Category = "Smart Phone"
+            }
+        };
+    }
+}
